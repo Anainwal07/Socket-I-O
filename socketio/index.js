78a23d63708c7a750d80faefd1a9ba9913871462
Ankit Nainwal <ankit.nainwal.3150@gmail.com>
2024-06-04 16:22:48 +0530

    Rooms

--- socketio/index.js
+++ socketio/index.js
@@ -20,25 +20,48 @@ app.get("/", (req, res) => {
 
 
 //custom-namespace
-var cnsp = io.of('/custom-namespace');
+// var cnsp = io.of('/custom-namespace');
 
-cnsp.on('connection' , (socket) => {
-    console.log('A user connected');
 
-    cnsp.emit('customEvent' , 'Tester event call') ; 
+// cnsp.on('connection' , (socket) => {
+//     console.log('A user connected');
+
+//     // cnsp.emit('customEvent' , 'Tester event call') ; 
     
-    // users++;
-    // socket.emit('newUserConnect' , {message : ' HI Welcome dear'});
+//     socket.join()
 
-    // socket.broadcast.emit('newUserConnect' , {message: users + "users connected"}) ;
+//     // users++;
+//     // socket.emit('newUserConnect' , {message : ' HI Welcome dear'});
+
+//     // socket.broadcast.emit('newUserConnect' , {message: users + "users connected"}) ;
+
+//     socket.on('disconnect' , () => {
+//         console.log("A user disconnected") ; 
+
+//     //     users--;
+//     //     socket.broadcast.emit('newUserConnect' , {message : users + ' users connected'});
+//      })
+// }); 
+var roomno = 1; 
+var full = 0 ; 
+
+io.on('connection', (socket) => {
+    console.log('A user connected' ) ; 
+
+    socket.join('room-' + roomno) ;
+
+    io.sockets.in('room-' + roomno).emit('connectedRoom' , "You are connected to room no " + roomno) ;
+
+    full++ ; 
+    if(full >= 2){
+        full = 0 ; 
+        roomno++ ; 
+    }
 
     socket.on('disconnect' , () => {
-        console.log("A user disconnected") ; 
-
-    //     users--;
-    //     socket.broadcast.emit('newUserConnect' , {message : users + ' users connected'});
-     })
-}); 
+        console.log('A user disconnected') ; 
+    }) ; 
+});
 
 server.listen(3000, () => {
     console.log("Server ready on PORT 3000");
